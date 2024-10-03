e8507538bf902a7bf533622c5716d6f35a73e2aa
Sachin Kumar <115858954+Sachinmca778@users.noreply.github.com>
2024-10-03 19:12:05 +0000

    uncommented code

--- Array/Array.java
+++ Array/Array.java
@@ -1,45 +1,45 @@
 //1. Print Our Marks increasing by 1.
 
-// public class Array{
+public class Array{
 
-//     public static void main(String args[]){
-//         int arr[] = {25,26,10};  //array created 
-//         updateMarks(arr);           //function used
-//         for(int i =0 ;i<arr.length;i++){
-//             System.out.print(arr[i]+ " ");
-//         }
-//     }
+    public static void main(String args[]){
+        int arr[] = {25,26,10};  //array created 
+        updateMarks(arr);           //function used
+        for(int i =0 ;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
 
-//     public static void updateMarks(int arr[]) {
-//         for(int i=0;i<arr.length;i++)  
-//         {
-//             arr[i]= arr[i]+1;
-//         }
+    public static void updateMarks(int arr[]) {
+        for(int i=0;i<arr.length;i++)  
+        {
+            arr[i]= arr[i]+1;
+        }
 
-//     }
-// }
+    }
+}
 
 
 //2. Linear Search 
 
-// public class Array {
-//     public static void main (String args[])
-//     {
-//         int arr[] = { 2,7,3,0,5};
-//       int  index = LinerSearchIndex(arr,3);
-//         System.out.println(index);
-//     }
+public class Array {
+    public static void main (String args[])
+    {
+        int arr[] = { 2,7,3,0,5};
+      int  index = LinerSearchIndex(arr,3);
+        System.out.println(index);
+    }
 
-//     public static int LinerSearchIndex (int arr[],int target)
-//     {
-//         for( int i=0;i<arr.length ;i++)
-//         {
-//             if(arr[i]==target)
-//             {
-//                 return i;
+    public static int LinerSearchIndex (int arr[],int target)
+    {
+        for( int i=0;i<arr.length ;i++)
+        {
+            if(arr[i]==target)
+            {
+                return i;
 
-//             }
-//         }
-//         return 0;
-//     }
-// }
+            }
+        }
+        return 0;
+    }
+}
