0b2184d58b7abfe58898a3d64379719e8ef0c6b8
Sachin <sachin@Narendras-MacBook-Air.local>
2024-11-21 00:53:08 +0530

    arrange the array question in formated

---
+++ Array/Array2.java
@@ -0,0 +1,23 @@
+// 2. Linear Search 
+
+public class Array2 {
+    public static void main (String args[])
+    {
+        int arr[] = { 2,7,3,0,5};
+      int  index = LinerSearchIndex(arr,3);
+        System.out.println(index);
+    }
+
+    public static int LinerSearchIndex (int arr[],int target)
+    {
+        for( int i=0;i<arr.length ;i++)
+        {
+            if(arr[i]==target)
+            {
+                return i;
+
+            }
+        }
+        return 0;
+    }
+}
