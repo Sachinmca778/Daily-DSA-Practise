cf129ee40458b01bb90024fbfbf25db2bec0486e
Sachin <sachin@Narendras-MacBook-Air.local>
2024-11-23 18:42:08 +0530

    solve Q8 Array

---
+++ Array/Array8.java
@@ -0,0 +1,31 @@
+//Check if an Array is Sorted
+
+
+// Input:
+// N = 5, array[] = {1,2,3,4,5}
+//Output :True
+
+
+public class Array8 {
+    public static void main(String[] args)
+    {
+        int arr[] = {1,3,5,6,7};
+        boolean b =checkSort(arr);
+        System.out.println(b);
+
+    }
+
+    public static boolean checkSort(int arr[])
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]>arr[i+1])      
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
